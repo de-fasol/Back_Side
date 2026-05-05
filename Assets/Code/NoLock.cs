515395b2cc9764d42a3812f707038c0d313f30d8
de_fasolka <speedyvoin@gmail.com>
2026-05-05 17:30:28 +1000

    Itch.io

---
+++ Assets/Code/NoLock.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Code
+{
+    public class NoLock : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        }
+
+    }
+}
